85406ff246ec9be9c964c956aa1322d97ccb88c0
reidroberts24 <116128525+reidroberts24@users.noreply.github.com>
2023-03-22 18:45:14 -0500

    Update Pond.java

--- Pond.java
+++ Pond.java
@@ -1,8 +1,28 @@
 public class Pond {
     // methods:
         // main method
-            // creates 4 Frog objects
-            // create 3 Fly objects
-            // perform operations 
+        public static void main(String[] args) {
+            Frog peepo = new Frog("Peepo");
+            Frog pepe = new Frog("Pepe",10,15);
+            Frog peepaw = new Frog("Peepaw",4.6,5);
+            Frog kermit = new Frog("Kermit", 25, 25);
+
+            Fly mosquito = new Fly(1,3);
+            Fly horsefly = new Fly(6);
+            Fly wasp = new Fly(10,10);
+
+            kermit.setSpecies("1331 Frogs");
+            System.out.println(peepo.toString());
+            peepo.eat(horsefly);
+            System.out.println(horsefly.toString());
+            peepo.grow(8);
+            peepo.eat(horsefly);
+            System.out.println(horsefly.toString());
+            System.out.println(peepo.toString());
+            System.out.println(kermit.toString());
+            peepaw.grow(4);
+            System.out.println(peepaw.toString());
+            System.out.println(pepe.toString());
+        }
     
     }
